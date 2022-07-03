10285fc5ca8ed60b802412e378b842422b0d1114
vmariano <m.vicent@gmail.com>
2022-07-03 17:44:36 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import users from "./routes/users";
+
+const app = express();
+
+app.use(express.static(__dirname + "/src"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/users", users);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server is listening on port ${process.env.PORT}`)
+);
---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.admin={};
+
+config.app={};
+
+config.admin.name="vmariano";
+config.admin.email="";
+config.app.name="admin-ts-example";
+config.app.url="";
+
+module.exports=config
